fce4f64af5e887ab4bed8918e27b8e6a7416d4c8
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-10-14 09:49:04 -0300

    Added transaction model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transaction(models.Model):
+    description = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    user = models.ForeignKey(User)
