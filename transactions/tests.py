e940a20f6b9fad2d759c96558bacaa1466324b99
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-10-14 10:50:24 -0300

    Added tests for allowed method

--- transactions/tests.py
+++ transactions/tests.py
@@ -25,7 +25,25 @@ class TransactionsUrlsTestCase(TestCase):
         resolver = self.resolve_by_name('transaction', pk=1)
 
         self.assertIn('get', resolver.func.actions)
-        self.assertEqual('retrieve', resolver.func.actions['get'])        
+        self.assertEqual('retrieve', resolver.func.actions['get'])
+
+    def test_list_url_only_allows_get_and_post(self):
+        resolver = self.resolve_by_name('transactions')
+
+        self.assertEqual(2, len(resolver.func.actions))
+        self.assertIn('get', resolver.func.actions)
+        self.assertIn('post', resolver.func.actions)
+
+    def test_single_url_allows_all_methods_except_post(self):
+        """All methods are: GET, PUT, PATCH and DELETE"""
+        resolver = self.resolve_by_name('transaction', pk=1)
+
+        self.assertEqual(4, len(resolver.func.actions))
+        self.assertIn('get', resolver.func.actions)
+        self.assertIn('put', resolver.func.actions)
+        self.assertIn('patch', resolver.func.actions)
+        self.assertIn('delete', resolver.func.actions)
+        
 
     def resolve_by_name(self, name, **kwargs):
         url = reverse(name, kwargs=kwargs)
--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import url
 from transactions import views
 
+list_actions = {
+    'get': 'list',
+    'post': 'create'
+}
+
+single_actions = {
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+}
+
 urlpatterns = [
-    url(r'^$', views.TransactionViewSet.as_view({ 'get': 'list' }), name="transactions"),
-    url(r'^(?P<pk>\d+)$', views.TransactionViewSet.as_view({ 'get': 'retrieve' }), name='transaction'),
+    url(r'^$', views.TransactionViewSet.as_view(list_actions), name="transactions"),
+    url(r'^(?P<pk>\d+)$', views.TransactionViewSet.as_view(single_actions), name='transaction'),
 ]
