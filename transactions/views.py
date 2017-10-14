821d930edf92696a7a1ec39196753ab02c064f11
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-10-14 10:05:22 -0300

    Setup serializers and views

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from transactions.models import Transaction
+from transactions.serializers import TransactionSerializer
 
-# Create your views here.
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
