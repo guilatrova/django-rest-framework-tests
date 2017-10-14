d5e809167ceb1023495afe2ed07d1e5f8dd47deb
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-10-14 13:30:57 -0300

    Added token authentication

--- drf_testing/settings.py
+++ drf_testing/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     
     'transactions'
 ]
@@ -51,6 +52,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
 
 ROOT_URLCONF = 'drf_testing.urls'
 
--- transactions/views.py
+++ transactions/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from transactions.models import Transaction
 from transactions.serializers import TransactionSerializer
 from transactions.permissions import IsOwner
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsOwner,)
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
